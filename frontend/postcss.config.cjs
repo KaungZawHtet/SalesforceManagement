f791aa38584acba7d9e7a828b7a683f39faf9669
Kaung Zaw Htet <kaungzawhtet.mm@gmail.com>
2026-08-08 00:11:55 +0700

    css fixed

---
+++ frontend/postcss.config.cjs
@@ -0,0 +1,3 @@
+module.exports = ***REMOVED***
+  plugins: [],
+***REMOVED***;
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+import tailwindcss from 'tailwindcss';
+
+export default tailwindcss(***REMOVED***
+  content: [
+    './src/**/*.***REMOVED***js,ts,jsx,tsx***REMOVED***',
+***REMOVED***,
+  themes: ***REMOVED***
+    extend: ***REMOVED******REMOVED***,
+  ***REMOVED***,
+  plugins: [],
+***REMOVED***);
